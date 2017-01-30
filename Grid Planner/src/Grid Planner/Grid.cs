d84d729d3d09b95e52cbc132dd33a661035ea6f1
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2017-01-30 21:33:36 +0100

    init .NET Core App

---
+++ Grid Planner/src/Grid Planner/Grid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grid_Planner
+{
+    public class Grid
+    {
+        public Grid()
+        {
+
+        }    
+             
+    }
+
+    /// <summary>
+    /// Schema for Grid point object
+    /// </summary>
+    class Point
+    {
+        int X { get; set; }
+        int Y { get; set; }
+
+
+    }
+}
---
+++ Grid Planner/src/Grid Planner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grid_Planner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
