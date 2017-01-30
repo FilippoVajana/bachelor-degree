b89c9954f53382ebae1c6adc5dd7ede134b28622
Filippo Vajana <filippo.vajana@studenti.unimi.it>
2017-01-30 22:07:47 +0100

    aggiunta classe Point

--- Grid Planner/src/Grid Planner/Grid.cs
+++ Grid Planner/src/Grid Planner/Grid.cs
@@ -5,23 +5,82 @@ using System.Threading.Tasks;
 
 namespace Grid_Planner
 {
-    public class Grid
+    interface IGrid
     {
-        public Grid()
-        {
+        int ManhattanDistance(Point p1, Point p2);
+        Point[] GetNeighbors(Point p);
+    }
 
-        }    
-             
+    public class Grid : IGrid
+    {
+        private int _sizeX, _sizeY;
+        private Point[][] _grid;
+
+        public Grid(int sizeX, int sizeY)
+        {
+            _sizeX = sizeX;
+            _sizeY = sizeY;
+            //_grid = BuildGrid();
+        }
+
+        public int ManhattanDistance(Point p1, Point p2)
+        {
+            return (Math.Abs(p1.X - p2.X) + Math.Abs(p1.Y - p2.Y));
+        }
+
+        public Point[] GetNeighbors(Point p)
+        {
+            if (isGridPoint(p))
+            {
+
+            }
+        }
+
+        private bool isGridPoint(Point p)
+        {
+            return (0 <= p.X && p.X < _sizeX) && (0 <= p.Y && p.Y < _sizeY);
+        }
     }
 
     /// <summary>
     /// Schema for Grid point object
     /// </summary>
-    class Point
+    public class Point
     {
-        int X { get; set; }
-        int Y { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
+        private int dangerLevel;
+        public int Danger
+        {
+            get
+            {
+                return dangerLevel;
+            }
+            set
+            {
+                if (0 <= value && value <= 10 )
+                {
+                    dangerLevel = value;
+                }
+            }
+        }
+
+        private int confidenceLevel;
+        public int Confidence
+        {
+            get
+            {
+                return confidenceLevel;
+            }
+            set
+            {
+                if (0 <= value && value <= 10)
+                {
+                    confidenceLevel = value;
+                }
+            }
+        }
 
     }
 }
